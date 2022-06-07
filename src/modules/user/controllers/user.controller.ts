25c7f3523842478eb022f4e081e39aa6a2d22cf2
JMJHOX <josemiguelaparicio507@gmail.com>
2022-06-07 00:18:54 -0500

    deprecating old endpoint getUsers

--- src/modules/user/controllers/user.controller.ts
+++ src/modules/user/controllers/user.controller.ts
@@ -9,22 +9,11 @@ import {
 } from '@nestjs/common';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
-import { PaginateResponse } from '../../../common/decorators';
-import { PageDto, PageOptionsDto } from '../../../common/dtos';
-import { UserDto } from '../dtos';
-import { UserService } from '../services';
-
 @UseInterceptors(ClassSerializerInterceptor)
 @Controller('User')
 @ApiTags('Users')
 export class UserController {
-  constructor(private readonly _userService: UserService) { }
+  constructor() { }
 
-  @Get()
-  @HttpCode(HttpStatus.OK)
-  @PaginateResponse(UserDto)
-  @ApiOperation({ summary: 'Get user list' })
-  async getUsers(@Query() options: PageOptionsDto): Promise<PageDto<UserDto>> {
-    return this._userService.getUsers(options);
-  }
+
 }
